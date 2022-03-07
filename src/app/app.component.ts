6e485025df79cefc0b67dec7531c9adaedbf9841
Bruno Araujo <bruno.aoliveira@al.infnet.edu.br>
2022-03-07 14:48:16 -0300

    entregando a atividade

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'projeto01';
+  numero: number = 0;
+
+  adicionar():void{
+    this.numero++
+  }
+
+  diminuir():void{
+    this.numero--
+  }
+
 }
